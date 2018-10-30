54b750b1dbd8012dda33c6b8dbf8c0852f54d507
BuildTools <unconfigured@null.spigotmc.org>
2018-10-30 00:19:19 -0400

    Add rotation module, and change scale module to be absolute instead of relative

---
+++ Assets/_Scripts/RotationModule.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class RotationModule : MonoBehaviour
+{
+    public bool m_canRotateFreely = false;
+    public float m_minRotationZ = 0.0f;
+    public float m_maxRotationZ = 0.0f;
+    
+    // Use this for initialization
+    void Start ()
+    {
+        if (m_minRotationZ > m_maxRotationZ)
+        {
+            throw new System.ArgumentOutOfRangeException(name, "Min rotation is higher than max rotation.");
+        }
+        if (m_maxRotationZ - m_minRotationZ > 360)
+        {
+            throw new System.ArgumentOutOfRangeException(name, "Rotation range wider than 360 degrees, please simplify.");
+        }
+        else if (m_maxRotationZ - m_minRotationZ == 360)
+        {
+            throw new System.ArgumentOutOfRangeException(name, "Use Can Rotate Freely instead.");
+        }
+        if (m_minRotationZ <= -360 || m_maxRotationZ > 360)
+        {
+            throw new System.ArgumentOutOfRangeException(name, "Rotation clamp(s) beyond +-360.  Please simplify.");
+        }
+        if (m_maxRotationZ < 0)
+        {
+            throw new System.ArgumentOutOfRangeException(name, "Max rotation less than zero.  Please simplify.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!m_canRotateFreely)
+        {
+            float relativeRange = (m_maxRotationZ - m_minRotationZ) / 2f;
+            float offset = m_maxRotationZ - relativeRange;
+            Vector3 angles = transform.eulerAngles;
+            float z = ((angles.z + 540) % 360) - 180 - offset;
+
+            if(Mathf.Abs(z) > relativeRange)
+            {
+                angles.z = relativeRange * Mathf.Sign(z) + offset;
+                transform.eulerAngles = angles;
+            }
+        }
+    }
+
+    public bool CanAdjustRotation()
+    {
+        return m_minRotationZ != m_maxRotationZ || m_canRotateFreely;
+    }
+}
--- Assets/_Scripts/ScaleModule.cs
+++ Assets/_Scripts/ScaleModule.cs
@@ -2,35 +2,32 @@
 
 public class ScaleModule : MonoBehaviour
 {
-    public Vector2 m_minScaleModifier = Vector2.one;
-    public Vector2 m_maxScaleModifier = Vector2.one;
-    public Vector2 m_currentScaleModifier = Vector2.one;
-
-    private Vector3 m_baseScale = Vector3.zero;
+    public Vector3 m_minScale = Vector3.one;
+    public Vector3 m_maxScale = Vector3.one;
 
     // Use this for initialization
     void Start ()
     {
-        m_baseScale = transform.localScale;
+        if(m_minScale.x > m_maxScale.x || m_minScale.y > m_maxScale.y)
+        {
+            throw new System.ArgumentOutOfRangeException(name, "Min scale is higher than max scale.");
+        }
     }
     
     // Update is called once per frame
     void Update ()
     {
-        m_currentScaleModifier = Vector2.Min(m_currentScaleModifier, m_maxScaleModifier);
-        m_currentScaleModifier = Vector2.Max(m_currentScaleModifier, m_minScaleModifier);
-
-        float xScale = m_baseScale.x * m_currentScaleModifier.x;
-        float yScale = m_baseScale.y * m_currentScaleModifier.y;
-        transform.localScale = new Vector3(xScale, yScale, m_baseScale.z);
+        Vector3 scale = Vector3.Min(transform.localScale, m_maxScale);
+        scale = Vector3.Max(scale, m_minScale);
+        transform.localScale = scale;
     }
 
-    public bool SetScaleModifier(float xScale, float yScale)
+    public bool SetScale(float xScale, float yScale)
     {
-        if(xScale >= m_minScaleModifier.x && yScale >= m_minScaleModifier.y &&
-            xScale <= m_maxScaleModifier.x && yScale <= m_maxScaleModifier.y)
+        if(xScale >= m_minScale.x && yScale >= m_minScale.y &&
+            xScale <= m_maxScale.x && yScale <= m_maxScale.y)
         {
-            m_currentScaleModifier.Set(xScale, yScale);
+            transform.localScale = new Vector3(xScale, yScale, transform.localScale.z);
             return true;
         }
         return false;
@@ -38,6 +35,6 @@ public class ScaleModule : MonoBehaviour
 
     public bool CanAdjustScale()
     {
-        return m_minScaleModifier != m_maxScaleModifier;
+        return m_minScale != m_maxScale;
     }
 }
